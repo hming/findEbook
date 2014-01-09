a9af2362f80ab67e0473b5a52c5327e1e6a7a4c5
hming <blues4@126.com>
2014-01-10 00:09:46 +0800

    Init project.

---
+++ findbook.py
@@ -0,0 +1,33 @@
+# -*- coding:UTF-8 -*-
+import cookielib
+import urllib
+import urllib2
+import re
+import sys
+
+import login
+import commentURL
+
+
+def getBookCategory():
+    """
+
+    get book category
+    :return: array of book category
+    """
+    rt = []
+    find_cat_re = re.compile(r'<li.+?<a href="(catalog.php.+?)">(.+?)</a>.+?</li>', re.DOTALL)
+    indexRequest = urllib2.Request(commentURL.URL_BASE)
+    index_page = urllib2.urlopen(indexRequest).read()
+    index_page = index_page.decode("UTF-8")
+    for cat in find_cat_re.findall(index_page):
+        rt.append({'cat_url': cat[0], 'cat_name': cat[1]})
+    return rt
+
+if __name__ == '__main__':
+    rt = getBookCategory()
+    print rt
+
+
+
+
---
+++ login.py
@@ -0,0 +1,39 @@
+# -*- coding:UTF-8 -*-
+import cookielib
+import urllib
+import urllib2
+
+import commentURL
+
+
+def login(userName, password):
+    LOGIN_SUCCESS_FLAG = 'logout.php'
+    cj = cookielib.LWPCookieJar()
+
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    urllib2.install_opener(opener)
+    parameter = {
+        "user": userName,
+        "pass": password,
+        "ret": commentURL.URL_BASE
+    }
+    req = urllib2.Request(
+        url=commentURL.URL_LOGIN,
+        data=urllib.urlencode(parameter)
+    )
+    jump = urllib2.urlopen(req)
+    rs_str = jump.read()
+    return rs_str.index(LOGIN_SUCCESS_FLAG) > -1
+
+
+def logout():
+    logout_request = urllib2.Request(commentURL.URL_LOGOUT)
+    try:
+        urllib2.urlopen(logout_request)
+        return True
+    except IOError:
+        return False
+
+
+
+
